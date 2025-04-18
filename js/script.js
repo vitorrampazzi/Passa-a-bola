45d397258e7d2c2423878ea698c5e3288e1804f0
vitorrampazzi <vitor.rampazzi@gmail.com>
2025-04-18 00:57:58 -0300

    Esboço do site do Passa-a-bola

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function scrollToCadastro() {
+    document.getElementById('cadastro').scrollIntoView({ behavior: 'smooth' });
+  }
+
+  function handleCadastro(event) {
+    event.preventDefault();
+    const msg = document.getElementById('mensagemCadastro');
+    msg.classList.remove('d-none');
+    setTimeout(() => msg.classList.add('d-none'), 3000);
+    event.target.reset();
+  }
